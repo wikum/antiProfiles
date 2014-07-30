ef68390931c3af3367f902ff77a74aab55f8ac04
hcorrada <hcorrada@gmail.com>
2014-07-29 23:19:35 -0400

    add code to calculate EV

---
+++ R/ev.R
@@ -0,0 +1,33 @@
+ev <- function(x, cutoff=NULL, plot=FALSE, ...) {
+  if (!is.matrix(x) || !is.numeric(x)) {
+    stop("argument 'x' must be a numeric matrix") 
+  }
+
+  if (!is.null(cutoff)) {
+    if (!is.numeric(cutoff)) {
+      stop("argument 'cutoff' must be numeric")
+    }
+
+    x[x<cutoff] <- NA
+  }
+  
+  mns <- rowMeans(x, na.rm=TRUE)
+  sds <- matrixStats::rowSds(x, na.rm=TRUE)
+
+  drop <- is.na(sds) & is.na(mns)
+  mns <- mns[!drop]
+  sds <- sds[!drop]
+  
+  fit <- locfit(sds^2 ~ lp(mns), family="gamma")
+  expSd <- sqrt(predict(fit, mns))
+
+  ev <- rep(NA, nrow(x))
+  ev[!drop] <- log2(sds) - log2(expSd)
+
+  if (plot) {
+    smoothScatter(mns, sds, xlab="mean expression", ylab="std. dev. expression", ...)
+    f1 <- function(x) sqrt(predict(fit,x))
+    curve(f1, from=min(mns), to=max(mns), col="red", add=TRUE)
+  }
+  ev
+}
